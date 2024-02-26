8972814c452e791d8b337a102345438f0a313792
DmytroKorolyov <148568611+DmytroKorolyov@users.noreply.github.com>
2024-02-26 16:01:40 +0200

    3

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -19,22 +19,6 @@ export default ImageGalleryItem;
 
 
 
-// import React from 'react';
-// import s from '../styles/styles.module.css'
-
-// const ImageGalleryItem = ({ image, onItemClick }) => {
-//   return (
-//     <li className={s.image_gallery_item}>
-//       <img className={s.image_gallery_item_image}
-//         src={image.webformatURL}
-//         alt={image.tags}
-//         onClick={() => onItemClick(image.largeImageURL)}
-//       />
-//     </li>
-//   );
-// };
-
-// export default ImageGalleryItem;
 
 
 
@@ -44,86 +28,3 @@ export default ImageGalleryItem;
 
 
 
-
-
-
-
-
-
-
-
-// import React from 'react';
-
-// class ImageGalleryItem extends React.Component {
-//   handleClick = () => {
-//     const { image, handleOpenModal } = this.props;
-//     handleOpenModal(image);
-//   };
-
-//   render() {
-//     const { image } = this.props;
-//     return (
-//       <li className="gallery-item" onClick={this.handleClick}>
-//         <img src={image.webformatURL} alt={image.tags} />
-//       </li>
-//     );
-//   }
-// }
-
-// export default ImageGalleryItem;
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-
-// const ImageGalleryItem = ({ image, handleOpenModal }) => {
-//   const handleClick = () => {
-//     handleOpenModal(image); // Викликаємо функцію для відкриття модального вікна
-//   };
-
-//   return (
-//     <li className="gallery-item" onClick={handleClick}>
-//       <img src={image.webformatURL} alt={image.tags} />
-//     </li>
-//   );
-// };
-
-// export default ImageGalleryItem;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from "react";
-
-// export const ImageGalleryItem = ({webformatURL, tags}) => {
-//     return <li> <img src={webformatURL} alt={tags} />
-        
-        
-//         </li>
-// }
